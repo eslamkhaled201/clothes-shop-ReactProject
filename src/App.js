8192e4133463a5b6d44a7a73a2d8cdb9c91057b2
eslamkhaled201 <59029234+eslamkhaled201@users.noreply.github.com>
2021-01-16 21:20:23 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,26 +4,26 @@ import HomePage from './pages/home-page/Hompage-componet';
 import { Route, Switch } from 'react-router-dom';
 import NavBar from './components/nav-bar/nav-bar';
 import Shop from './pages/shop-page/shop';
-import {SignInSignUpPage } from './pages/signIn-sign-up-page/signIn-sign-up';
+import { SignInSignUpPage } from './pages/signIn-sign-up-page/signIn-sign-up';
 
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
-    this.state ={
-      currentUser : null
+    this.state = {
+      currentUser: null
     }
   }
- 
+
   render() {
     return (
       <div className="App ">
         <NavBar currentUser={this.state.currentUser}></NavBar>
         <div className='main'>
           <Switch>
-            <Route exact path='/' component={HomePage} />
-            <Route exact path='/shop' component={Shop} />
-            <Route exact path='/logIn' component={SignInSignUpPage}  />
+            <Route exact path='https://eslamkhaled201.github.io/clothes-shop-ReactProject/' component={HomePage} />
+            <Route exact path='https://eslamkhaled201.github.io/clothes-shop-ReactProject/shop' component={Shop} />
+            <Route exact path='https://eslamkhaled201.github.io/clothes-shop-ReactProject/logIn' component={SignInSignUpPage} />
           </Switch>
         </div>
       </div>
